1efdbd7435c625841c7bf6fded03a92e9852d05b
Mark Chapman <mark.chapman@gmail.com>
2015-04-01 15:22:58 +0100

    Shows that problem is within angular-material.  Need to go out now but will try and fix later.

--- protractor.conf.js
+++ protractor.conf.js
@@ -8,6 +8,7 @@ exports.config = {
   capabilities: {
     'browserName': 'chrome'
   },
+  directConnect: true,
 
   seleniumAddress: 'http://localhost:4444/wd/hub',
   baseUrl: 'http://localhost:8000/examples/',
--- tests/integration/example.js
+++ tests/integration/example.js
@@ -3,7 +3,7 @@
 describe('example', function() {
   beforeEach(function() {
     browser.driver.get('http://localhost:8000/examples/index.html');
-    browser.driver.wait(browser.driver.isElementPresent(by.id("form")), 5000);
+    //browser.driver.wait(browser.driver.isElementPresent(by.id("form")), 5000);
   });
 
   describe('manual markup', function() {
